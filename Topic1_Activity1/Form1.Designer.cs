b527433900296ddc17907bb835f8f93a4a8d635b
Sambro09 <116610371+Sambro09@users.noreply.github.com>
2023-03-03 14:22:46 -0800

    Pressing button will toggle label text

--- Topic1_Activity1/Form1.Designer.cs
+++ Topic1_Activity1/Form1.Designer.cs
@@ -28,18 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            buttonLabel = new Label();
+            clickHereButton = new Button();
+            surpriseLabel = new Label();
             SuspendLayout();
+            // 
+            // buttonLabel
+            // 
+            buttonLabel.AutoSize = true;
+            buttonLabel.Location = new Point(171, 41);
+            buttonLabel.Name = "buttonLabel";
+            buttonLabel.Size = new Size(159, 20);
+            buttonLabel.TabIndex = 0;
+            buttonLabel.Text = "Press to reveal surprise";
+            // 
+            // clickHereButton
+            // 
+            clickHereButton.Location = new Point(196, 75);
+            clickHereButton.Name = "clickHereButton";
+            clickHereButton.Size = new Size(94, 29);
+            clickHereButton.TabIndex = 1;
+            clickHereButton.Text = "Click here";
+            clickHereButton.UseVisualStyleBackColor = true;
+            clickHereButton.Click += clickHereButton_Click;
+            // 
+            // surpriseLabel
+            // 
+            surpriseLabel.BorderStyle = BorderStyle.FixedSingle;
+            surpriseLabel.Location = new Point(185, 127);
+            surpriseLabel.Name = "surpriseLabel";
+            surpriseLabel.Size = new Size(120, 25);
+            surpriseLabel.TabIndex = 2;
+            surpriseLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(503, 245);
+            Controls.Add(surpriseLabel);
+            Controls.Add(clickHereButton);
+            Controls.Add(buttonLabel);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label buttonLabel;
+        private Button clickHereButton;
+        private Label surpriseLabel;
     }
 }
--- Topic1_Activity1/Form1.cs
+++ Topic1_Activity1/Form1.cs
@@ -1,10 +1,27 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Topic1_Activity1
 {
     public partial class Form1 : Form
     {
+        public bool isClicked = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void clickHereButton_Click(object sender, EventArgs e)
+        {
+            isClicked = !isClicked;
+
+            if (isClicked)
+            {
+                surpriseLabel.Text = "Surprise!";
+            }
+            else
+            {
+                surpriseLabel.Text = "";
+            }
         }
     }
 }
